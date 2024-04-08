5ce1f980375dfab7aa93281ccd684d420dbfed26
Romel37151 <dsan37151@gmail.com>
2024-04-07 21:42:57 -0500

    reto 34

--- conexionbdd/src/main/java/com/krakendev/persistencia/test/TestConsultaArgumento.java
+++ conexionbdd/src/main/java/com/krakendev/persistencia/test/TestConsultaArgumento.java
@@ -6,13 +6,13 @@ import com.krakedev.persistencia.entidades.Personas;
 import com.krakedev.persistencia.servicios.AdminPersonas;
 import com.krakendev.persistencia.entidades.Persona;
 
-public class TestConsultaCedula {
+public class TestConsultaArgumento {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Persona personas= new Persona();
 		try {
-			 personas=AdminPersonas.buscarCedula("1105232977");
+			personas= AdminPersonas.buscarArgumentos("jonathan", "zhiña");
 			System.out.println(personas);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
--- conexionbdd/src/main/java/com/krakendev/persistencia/test/TestConsultasCedula.java
+++ conexionbdd/src/main/java/com/krakendev/persistencia/test/TestConsultasCedula.java
@@ -6,13 +6,13 @@ import com.krakedev.persistencia.entidades.Personas;
 import com.krakedev.persistencia.servicios.AdminPersonas;
 import com.krakendev.persistencia.entidades.Persona;
 
-public class TestConsultaArgumentos {
+public class TestConsultasCedula {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Persona personas= new Persona();
 		try {
-			personas= AdminPersonas.buscarArgumentos("jonathan", "zhiña");
+			 personas=AdminPersonas.buscarCedula("1105232977");
 			System.out.println(personas);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
